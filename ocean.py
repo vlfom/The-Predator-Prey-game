2cfef076a5ac135f0bbb670fa54f4fac026eaf33
Volodymyr Fomenko <vlfomenk@gmail.com>
2016-03-20 15:57:09 +0200

    Refactor code.

--- ocean.py
+++ ocean.py
@@ -14,7 +14,6 @@ class Obstacle:
 
 class Prey:
     """A prey class.
-
     Attributes:
         food_value   The amount energy that predator will earn after eating a single prey.
         spawn_time   The amount of time before prey tries to reproduce offspring.
@@ -34,7 +33,6 @@ class Prey:
 
 class Predator:
     """A predator class.
-
     Attributes:
         energy       The amount of time before concrete predator dies without food.
         spawn_time   The amount of time before predator tries to reproduce offspring.
@@ -67,12 +65,12 @@ class Cell:
 
 class Ocean:
     """A grid with predators and prey model.
-
     Attributes:
         h            Grid height
         w            Grid width
         spawn_rate   The amount of time before new generation is born.
         _dirs        Possible directions of prey and predators
+        _grid        Array with markers of whether animal has been modified on current step
     """
     _dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
@@ -117,62 +115,67 @@ class Ocean:
             i_, j_ = i + dir_[0], j + dir_[1]
             if 0 <= i_ < self.h and 0 <= j_ < self.w and \
                self.grid[i_][j_].obj is None:
-                if isinstance(self.grid[i][j].obj, Prey):
-                    self.grid[i_][j_].obj = Prey()
-                else:
-                    self.grid[i_][j_].obj = Predator()
+                self.grid[i_][j_].obj = type(self.grid[i][j].obj)()
                 return i_, j_
 
     def tick(self):
         """Modify ocean state after single time unit"""
 
-        grid_ = [[0] * self.w] * self.h
+        self.grid_ = [[0] * self.w] * self.h
         for i in range(self.h):
             for j in range(self.w):
-                if grid_[i][j] == 0 and \
-                        (isinstance(self.grid[i][j].obj, Predator) or
-                         isinstance(self.grid[i][j].obj, Prey)):
-                    dir_ = self._random.choice(self._dirs)
+                if self.grid_[i][j] == 0 and \
+                  (isinstance(self.grid[i][j].obj, Predator) or
+                   isinstance(self.grid[i][j].obj, Prey)):
+                    self.make_animal_turn(i, j)
 
-                    i_, j_ = i + dir_[0], j + dir_[1]
+    def make_animal_turn(self, i, j):
+        """Process one turn for animal on cell with coordinates (i, j)"""
 
-                    if 0 <= i_ < self.h and 0 <= j_ < self.w:
-                        if isinstance(self.grid[i][j].obj, Predator) and \
-                           isinstance(self.grid[i_][j_].obj, Prey):
-                            self.grid[i][j].obj.energy += self.grid[i_][j_].obj.food_value
-                            self.grid[i_][j_].obj = None
+        dir_ = self._random.choice(self._dirs)
 
-                        if self.grid[i_][j_].obj is None:
-                            self.grid[i_][j_].obj = self.grid[i][j].obj
-                            self.grid[i][j].obj = None
-                            grid_[i_][j_] = 1
-                        else:
-                            i_, j_ = i, j
+        i_, j_ = i + dir_[0], j + dir_[1]
 
-                        self.grid[i_][j_].obj.tick()
+        if 0 <= i_ < self.h and 0 <= j_ < self.w:
+            if isinstance(self.grid[i][j].obj, Predator) and \
+               isinstance(self.grid[i_][j_].obj, Prey):
+                self.grid[i][j].obj.energy += self.grid[i_][j_].obj.food_value
+                self.grid[i_][j_].obj = None
 
-                        if self.grid[i_][j_].obj.spawn_time == 0:
-                            self.grid[i_][j_].obj.spawn_time = self.spawn_rate
-                            cords = self.try_spawn(i_, j_)
-                            if cords is not None:
-                                grid_[cords[0]][cords[1]] = 1
+            if self.grid[i_][j_].obj is None:
+                self.grid[i_][j_].obj = self.grid[i][j].obj
+                self.grid[i][j].obj = None
+                self.grid_[i_][j_] = 1
+            else:
+                i_, j_ = i, j
 
-                        if isinstance(self.grid[i_][j_].obj, Predator) and \
-                           self.grid[i_][j_].obj.energy == 0:
-                            self.grid[i_][j_].obj = None
+            self.grid[i_][j_].obj.tick()
+
+            if self.grid[i_][j_].obj.spawn_time == 0:
+                self.grid[i_][j_].obj.spawn_time = self.spawn_rate
+                cords = self.try_spawn(i_, j_)
+                if cords is not None:
+                    self.grid_[cords[0]][cords[1]] = 1
+
+            if isinstance(self.grid[i_][j_].obj, Predator) and \
+               self.grid[i_][j_].obj.energy == 0:
+                self.grid[i_][j_].obj = None
 
     def get_animals_count(self):
         """Get count of prey and predators in ocean"""
 
-        prey_count, pred_count = 0, 0
+        objects_count = {
+            'Prey': 0,
+            'Predator': 0,
+            'Obstacle': 0,
+            'NoneType': 0
+        }
+
         for i in range(self.h):
             for j in range(self.w):
-                if isinstance(self.grid[i][j].obj, Prey):
-                    prey_count += 1
-                elif isinstance(self.grid[i][j].obj, Predator):
-                    pred_count += 1
+                objects_count[type(self.grid[i][j].obj).__name__] += 1
 
-        return prey_count, pred_count
+        return objects_count['Prey'], objects_count['Predator']
 
 
 def init_ocean(h, w, pred_vitality, prey_food_value, spawn_rate, seed):
